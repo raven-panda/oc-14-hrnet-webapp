375e0b9b70622258b51d964b8f953864736cfe3b
raven-panda <ambroise.maire7@gmail.com>
2025-11-28 13:54:10 +0100

    feat(mocked data): mock data in local storage only if ENABLE_DATA_MOCK and FIXTURES are enabled

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import populateEmployeesLocalStorage from "./data/local-storage-populator/EmployeeLocalStoragePopulator.ts";
 
-populateEmployeesLocalStorage();
+if (import.meta.env.VITE_ENABLE_FIXTURE === "true" && import.meta.env.VITE_ENABLE_DATA_MOCK === "true") {
+  populateEmployeesLocalStorage();
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
