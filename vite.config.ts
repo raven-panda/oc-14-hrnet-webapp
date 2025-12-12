e5328d8d2d0ac86b54b54a03e138ba2120aa0fc7
raven-panda <ambroise.maire7@gmail.com>
2025-12-12 14:20:58 +0100

    chore(deployment): removed gh pages, adapted config for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const base = env.VITE_BASE || '/'
-
-  return {
-    base,
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
 });
